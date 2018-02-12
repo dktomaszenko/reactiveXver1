b6af89c504978501f98479aa657ac414c8ca957b
dktomaszenko <xjxxwbm9fx>
2018-02-12 15:30:18 +0100

    tutorial gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,30 @@
-var gulp = require('gulp');
 
 
+let gulp = require('gulp');
+
+//Plugins
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglify');
+let rename = require('gulp-rename');
+let serve  = require('gulp-serve');
+
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('app/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('app/*.js', ['scripts']);
+});
+
+gulp.task('serve', serve('.'));
+
+// Default Task
+gulp.task('default', ['scripts', 'serve', 'watch']);
